f6179e76ca3a5f27638b046af2f41f2516d93843
dlivas <dimitris.livas@gmail.com>
2019-08-28 17:28:38 +0100

    add db.hs level07 solutions

--- src/Level07/DB.hs
+++ src/Level07/DB.hs
@@ -18,22 +18,34 @@ import qualified Data.Text                          as Text
 
 import           Data.Time                          (getCurrentTime)
 
-import           Database.SQLite.Simple             (Connection, FromRow,
-                                                     Query (fromQuery), ToRow)
+import           Database.SQLite.Simple             ( Connection
+                                                    , FromRow
+                                                    , Query (fromQuery)
+                                                    , ToRow
+                                                    )
+
 import qualified Database.SQLite.Simple             as Sql
 
 import qualified Database.SQLite.SimpleErrors       as Sql
 import           Database.SQLite.SimpleErrors.Types (SQLiteResponse)
 
-import           Level07.AppM                      (App, Env (envDB))
+import           Level07.AppM                       ( App
+                                                    , AppM (..)
+                                                    , Env (envDB)
+                                                    , liftEither
+                                                    )
 
-import           Level07.Types                     (Comment, CommentText,
-                                                     DBFilePath (getDBFilePath),
-                                                     Error (DBError),
-                                                     FirstAppDB (FirstAppDB, dbConn),
-                                                     Topic, fromDBComment,
-                                                     getCommentText, getTopic,
-                                                     mkTopic)
+import           Level07.Types                      ( Comment
+                                                    , CommentText
+                                                    , DBFilePath (getDBFilePath)
+                                                    , Error (DBError)
+                                                    , FirstAppDB (FirstAppDB, dbConn)
+                                                    , Topic
+                                                    , fromDBComment
+                                                    , getCommentText
+                                                    , getTopic
+                                                    , mkTopic
+                                                    )
 
 -- Quick helper to pull the connection and close it down.
 closeDB
@@ -63,37 +75,69 @@ initDB fp = Sql.runDBAction $ do
 getDBConn
   :: App Connection
 getDBConn =
-  error "getDBConn not implemented"
+  AppM $ return . Right . dbConn . envDB
 
 runDB
   :: (a -> Either Error b)
   -> (Connection -> IO a)
   -> App b
-runDB =
-  error "runDB not re-implemented"
+runDB f cf =
+  AppM $ fmap f . cf . dbConn . envDB
 
 getComments
   :: Topic
   -> App [Comment]
-getComments =
-  error "Copy your completed 'getComments' and refactor to match the new type signature"
+getComments t = 
+  runDB
+    (traverse fromDBComment)
+    $ \conn -> Sql.query conn q (Sql.Only . getTopic $ t)
+  where
+    -- Write the query with an icky string and remember your placeholders!
+    -- To be doubly and triply sure we've no garbage in our response, we take care
+    -- to convert our DB storage type into something we're going to share with the
+    -- outside world. Checking again for things like empty Topic or CommentText values.
+    q = "SELECT id,topic,comment,time FROM comments WHERE topic = ?"
 
 addCommentToTopic
   :: Topic
   -> CommentText
   -> App ()
-addCommentToTopic =
-  error "Copy your completed 'appCommentToTopic' and refactor to match the new type signature"
+addCommentToTopic t c = do
+  -- Record the time this comment was created.
+  nowish <- liftIO getCurrentTime
+  -- We use the execute function this time as we don't care about anything
+  -- that is returned. The execute function will still return the number of rows
+  -- affected by the query, which in our case should always be 1.
+  runDB
+    Right
+    $ \conn -> Sql.execute conn q (getTopic t, getCommentText c, nowish)
+  -- An alternative is to write a returning query to get the Id of the DBComment
+  -- we've created. We're being lazy (hah!) for now, so assume awesome and move on.
+  where
+    -- Note the triple, matching the number of values we're trying to insert, plus
+    -- one for the table name.
+    -- Remember that the '?' are order dependent so if you get your input
+    -- parameters in the wrong order, the types won't save you here. More on that
+    -- sort of goodness later.
+    q = "INSERT INTO comments (topic,comment,time) VALUES (?,?,?)"
 
 getTopics
   :: App [Topic]
 getTopics =
-  error "Copy your completed 'getTopics' and refactor to match the new type signature"
+  runDB
+    (traverse ( mkTopic . Sql.fromOnly ))
+    $ \conn -> Sql.query_ conn q
+  where
+    q = "SELECT DISTINCT topic FROM comments"
 
 deleteTopic
   :: Topic
   -> App ()
-deleteTopic =
-  error "Copy your completed 'deleteTopic' and refactor to match the new type signature"
+deleteTopic t =
+  runDB
+    Right
+    $ \conn -> Sql.execute conn q (Sql.Only . getTopic $ t)
+  where
+    q = "DELETE FROM comments WHERE topic = ?"
 
 -- Go on to 'src/Level07/Core.hs' next.
